7b033d9695d98b8c2d67c6b4198f0b9b08663c08
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2022-12-20 09:49:12 -0300

    setup: mongodb connection set

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(3001, () => {
-	console.log('🚀 Server is running on http://localhost:3001');
+mongoose.connect('mongodb://localhost:27017')
+	.then(() => console.log('conectado ao mongo'))
+	.catch(() => console.log('Erro ao conectar no mongodbb'));
+
+
+const port = 3001;
+
+app.listen(port, () => {
+	console.log(`🚀 Server is running on http://localhost:${port}`);
 });
