f704126024d94895bf4fdbda5599b7fadf767ba0
Gabriel Cesarino <61214104+gabrielCesarino@users.noreply.github.com>
2022-12-20 10:12:38 -0300

    add: mongo db models

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,16 @@ import mongoose from 'mongoose';
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017')
-	.then(() => console.log('conectado ao mongo'))
-	.catch(() => console.log('Erro ao conectar no mongodbb'));
+	.then(() => {
+		const app = express();
+		const port = 3001;
+
+		app.listen(port, () => {
+			console.log(`🚀 Server is running on http://localhost:${port}`);
+		});
+	})
+	.catch (() => console.log('Erro ao conectar no mongodbb'));
 
 
-const port = 3001;
 
-app.listen(port, () => {
-	console.log(`🚀 Server is running on http://localhost:${port}`);
-});
+
